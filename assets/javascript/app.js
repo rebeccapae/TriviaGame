b7249787582387179bb0f3e0533ba0a51060ca39
Rebecca Pae <r.pae92@gmail.com>
2019-04-11 22:45:41 -0700

    Completed logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,13 @@
 $(document).ready(function() {
 
-var scoreCount = 0;
-var questionCount = -1;
-var clicked = false;
-var seconds = 0;
-var userChoice = "";
-
-var triviaBank = [
+    var scoreCount = 0;
+    var questionCount = -1;
+    var clicked = false;
+    var seconds = 0;
+    var secondsCounter;
+    var userChoice = ""
+    var secondsRem = 10;
+    var triviaBank = [
     {
         question: "How many cocoa beans do you need to make one pound of chocolate?",
         correctAnswer: "400",
@@ -37,96 +38,132 @@ var triviaBank = [
         correctAnswer: "Frozen",
         answerChoices: ["Creamy", "Cold", "Smooth", "Frozen"]
     },
-];
+    ];
 
+    $("#total-score").hide();
 
-$("#start-game").on("click", function() {
-    $("#start-game").hide();
-    showQuestion();
-});
+    $("#start-game").on("click", function() {
+        $("#start-game").hide();
+        showQuestion();   
+    });
 
+    function displayTimer() {
+        $("#time-remaining").text("seconds remaining: " + secondsRem);
+    };
 
-function startOver() {
-    
-};
-
-function displayTimer() {
-    $("#time-remaining").html("<img src='assets/images/timer2.gif' width='200px'/>");
-}
-
-function secondsCount(){
-    seconds ++;
-    console.log(seconds);
-}
-
-function stopTimer(){
-    clearTimeout(displayTimer);
-    clearInterval(secondsCount);
-}
-
-
-
-function showQuestion() {
-
-    // Increase questionCount by 1
-    questionCount ++;
-    var question = $("<div>").text(triviaBank[questionCount].question)
-    $("#question").append(question);
-
-    var userChoice = ""
-        
-        // for every question, show the 4 answer choices as buttons
-        for (var i = 0; i <4; i++){
-            var answerButtons = $("<button>");
-            answerButtons.addClass("answerChoiceButtons")
-            answerButtons.text(triviaBank[questionCount].answerChoices[i]);
-            $("#question").append(answerButtons);
-        };
-
-        // To run the timer gif and increase seconds count by 1 each second
-        setTimeout(displayTimer, 0);
-        setInterval(secondsCount, 1000);
-
-        // If the user clicks a button within 10 seconds, evaluate. If not, show the next question.
-        if (seconds < 10 && clicked == true) {
-            if (userChoice === triviaBank[questionCount].correctAnswer) {
-                scoreCount ++;
-                console.log("user wins: " + scoreCount);
-            }
-            else {
-                if (questionCount < triviaBank.length -1) {
-                showQuestion(); }
-                else {
-                    stopGame();
-                };
-            }
+    function secondsCount(){
+        seconds ++;
+        secondsRem --;
+        console.log("seconds: " + seconds);
+        if (seconds > 10) {
+            console.log("seconds = 10")
+            stopTimer();
+            clicked = false;
+            $("#question-box").text("Time's Up! The correct answer is: ");
+            showAnswer();
+            console.log("clicked: " + clicked);
+            setTimeout(showQuestion, 2000);
         }
         else {
-            alert("Out of Time");
-            stopTimer();
-           
-        }
+            console.log("under 10 seconds")
+        };
+    };
 
-        // Store the user's choice into a variable
-        $(".answerChoiceButtons").on("click", function() {
-            clicked = true;
-            userChoice = $(this).text();
-            console.log(userChoice);
+    function startTimer(){
+        setTimeout(displayTimer, 0);
+        secondsCounter = setInterval(secondsCount, 1000);
+        secondsRemaining = setInterval(displayTimer, 1000);
+    };
 
-        // Compare the user's answer with the correct answer
+    function stopTimer(){
+        clearInterval(secondsCounter);
+        clearInterval(secondsRemaining);
+        seconds = 0;
+        secondsRem = 10;
+        console.log("seconds stopped: " + seconds)
         
-        });
-    
+    };
+
+    function endGame() {
+        $("#total-score").show();
+        $("#total-score").text("Good job for finishing this quiz. You got: " + scoreCount + "/6 questions right!");
+        $("#question-box").empty();
+        $("#answer").empty();
+        $("#time-remaining").empty();
+        var startOver = $("<button>");
+        startOver.addClass("startover");
+        startOver.text("Start Over?");
+        $("#question-box").append(startOver);
+        $(".startover").on("click", function(){
+            scoreCount = 0;
+            questionCount = -1;
+            clicked = false;
+            seconds = 0;
+            secondsCounter;
+            userChoice = ""
+            secondsRem = 10;
+            showQuestion();
+            $("#total-score").hide();
+        }); 
+    };
+
+    function showAnswer() {
+        var correctAnswer = $("<div>");
+        correctAnswer.text(triviaBank[questionCount].correctAnswer);
+        $("#question-box").append(correctAnswer);
+        console.log("correct answer: " + triviaBank[questionCount].correctAnswer)
 
         
     };
 
-function nextQuestion() {
+    function showQuestion() {
 
-}
+        // Increase questionCount by 1
+        questionCount ++;
 
-function stopGame() {
+        if (questionCount == 6) {
+            endGame();
+        }
+
+        console.log("questionCount: " + questionCount);
+        $("#question-box").text(triviaBank[questionCount].question)
+
+        
+        
     
-}
+        // for every question, show the 4 answer choices as buttons
+        $("#answer").empty();
+        for (var i = 0; i <4; i++){
+            var answerButtons = $("<button>");
+            answerButtons.addClass("answerChoiceButtons");
+            answerButtons.attr("buttonValue", triviaBank[questionCount].answerChoices[i])
+            answerButtons.text(triviaBank[questionCount].answerChoices[i]);
+            $("#answer").append(answerButtons);
+        };
+
+        startTimer();
+
+        $(".answerChoiceButtons").on("click", function() {
+            clicked = true;
+            stopTimer();
+            userChoice = $(this).text();
+            console.log("user choice: " + userChoice);
+            console.log("clicked: " + clicked)
+
+            if (userChoice === triviaBank[questionCount].correctAnswer) {
+                var rightAnswer = $("<p>").text("You got it right!")
+                $("#question-box").append(rightAnswer);
+                setTimeout(showQuestion, 2000);
+                scoreCount ++;
+            }
+            else {
+                var wrongAnswer = $("<p>").text("Wrong answer. The correct answer is :")
+                $("#question-box").append(wrongAnswer);
+                showAnswer();
+                setTimeout(showQuestion, 2000);
+            }
+        
+        });
+    };
 
 });
